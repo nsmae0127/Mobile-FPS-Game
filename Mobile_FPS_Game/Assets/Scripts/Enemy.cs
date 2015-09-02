b17c0b0bfdea389987c1594391768fccb273ef09
Alvin Lee <nsmae0127@gmail.com>
2015-09-02 23:41:47 +0900

    Create an enemy

---
+++ Mobile_FPS_Game/Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+	[SerializeField]private float speed;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector2 position = transform.position;
+
+		position = new Vector2 (position.x - speed * Time.deltaTime, position.y);
+
+		transform.position = position;
+
+		Vector2 min = Camera.main.ViewportToWorldPoint (new Vector2 (0, 0));
+
+		if (transform.position.x < min.x) {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Mobile_FPS_Game/Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawn : MonoBehaviour
+{
+	public GameObject enemyPrefab;
+
+	private float spawnDelay = 5f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Invoke ("SpawnEnemy", spawnDelay);
+
+		// increase spawn rate every 30 seconds.
+		InvokeRepeating ("IncreaseSpawnRate", 0f, 30f);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void SpawnEnemy ()
+	{
+//		Vector2 min = Camera.main.ViewportToWorldPoint (new Vector2 (0, 0));
+		Vector2 max = Camera.main.ViewportToWorldPoint (new Vector2 (1, 1));
+
+		GameObject enemy = (GameObject)Instantiate (enemyPrefab);
+		enemy.transform.position = new Vector2 (max.x, -5.28f);
+
+		NextEnemySpawn ();
+	}
+
+	void NextEnemySpawn ()
+	{
+		float spawnDelays;
+
+		if (spawnDelay > 1f) {
+			spawnDelays = Random.Range (1f, spawnDelay);
+		} else {
+			spawnDelays = 1f;
+		}
+
+		Invoke ("SpawnEnemy", spawnDelays);
+	}
+
+	void IncreaseSpawnRate ()
+	{
+		if (spawnDelay > 1f)
+			spawnDelay--;
+
+		if (spawnDelay == 1f)
+			CancelInvoke ("IncreaseSpawnRate");
+	}
+}
